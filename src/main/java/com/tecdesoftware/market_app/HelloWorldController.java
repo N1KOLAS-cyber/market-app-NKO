20866b54c423bf95ace9e1ae3d2c3590fc41d166
Nicolas <gamboanicolas58@gmail.com>
2025-05-14 13:19:05 -0600

    Adding pdn and new profiel

--- src/main/java/com/tecdesoftware/market_app/HelloWorldController.java
+++ src/main/java/com/tecdesoftware/market_app/HelloWorldController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class HelloWorldController {
 
-    @GetMapping
+    @GetMapping("/hola")
     public String saludar(){
         return "Hello World and you NKO!💕";
     }}
